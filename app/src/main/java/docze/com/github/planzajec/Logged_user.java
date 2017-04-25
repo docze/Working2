b58139b14e097ca1ac5f64d43982677c7293104b
MarLunk <m.lunkiewicz@gmail.com>
2017-04-25 17:04:21 +0200

    Fundaments for Logged_user_activity * Add basic logged_user_activity * After correctly log in to website goes to new activity * ReConfgiuration AndroidManifest

---
+++ app/src/main/java/docze/com/github/planzajec/Logged_user.java
@@ -0,0 +1,13 @@
+package docze.com.github.planzajec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logged_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_user);
+    }
+}
